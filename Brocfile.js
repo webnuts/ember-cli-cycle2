fe9661791c7b93c3c0c4c212a96efff2a9caad8e
Martin Thøgersen <martin@webnuts.com>
2015-03-07 12:50:44 +0100

    Import cycle2 dependency

--- Brocfile.js
+++ Brocfile.js
@@ -18,4 +18,8 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import("bower_components/jquery-cycle2/build/jquery.cycle2.js");
+app.import("bower_components/jquery-cycle2/build/plugin/jquery.cycle2.carousel.min.js");
+app.import("bower_components/jquery-cycle2/build/plugin/jquery.cycle2.swipe.min.js");
+
 module.exports = app.toTree();
